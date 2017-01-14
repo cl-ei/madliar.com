a791bbeb96147c0fc5db6fc19c7a596a3cdd9efc
Caoliang <i@caoliang.net>
2017-01-14 17:35:20 +0800

    delete annotations

--- application/views.py
+++ application/views.py
@@ -12,28 +12,3 @@ def index(request):
 
 def server():
     pass
-
-
-
-# '__getitem__', '__gt__',
-# '__hash__', '__init__', '__iter__', '__le__', '__len__',
-# '__lt__', '__ne__', '__new__', '__reduce__',
-# '__reduce_ex__', '__repr__', '__setattr__',
-# '__setitem__', '__sizeof__',
-# '__str__', '__subclasshook__',
-# 'clear',
-# 'copy',
-# 'fromkeys',
-# 'get',
-# 'has_key',
-# 'items',
-# 'iteritems',
-# 'iterkeys',
-# 'itervalues',
-# 'keys', 'pop',
-# 'popitem',
-# 'setdefault',
-# 'update',
-# 'values',
-# 'viewitems', 'viewkeys', 'viewvalues']
-
