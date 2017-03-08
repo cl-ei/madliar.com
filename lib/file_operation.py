7ad32bf291d3d73e89df71ac47332e59eee096a2
Caoliang <i@caoliang.net>
2017-03-08 21:36:34 +0800

    reduce code

--- lib/file_operation.py
+++ lib/file_operation.py
@@ -1,15 +1,35 @@
-#!/usr/bin/env python
+# -*- coding:utf-8 -*-
 
-"""
-A unity script build for safely reducing the size of html code.
-
-Not finished yet!
-"""
-
+import os
 import re
+
+def search(path, f_type):
+    """
+    To find all files with certain file extension in a path.
+    """
+    f_type = f_type.lower()
+
+    file_list = []
+    for _ in os.listdir(path):
+        full_path = os.path.join(path, _)
+
+        if os.path.isfile(full_path):
+            _, file_ext = os.path.splitext(full_path)
+            if file_ext.lstrip(".").lower() == f_type:
+                file_list.append(full_path)
+
+        if os.path.isdir(full_path):
+            file_list += search(full_path, f_type)
+
+    return file_list
 
 
 def _split(content):
+	"""
+	A unity script build for safely reducing the size of html code.
+
+	Not finished yet!
+	"""
     inside = False
     snap = list()
     tag_list = list()
