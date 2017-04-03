30fac8f43db4ead73206724d75fe97457757837a
Caoliang <i@caoliang.net>
2017-04-03 21:43:05 +0800

    add template

--- main.py
+++ main.py
@@ -5,5 +5,10 @@ Entrance to run this script.
 from wsgiserver import management
 
 if __name__ == "__main__":
-    management.execute_from_command_line()
+    # management.execute_from_command_line()
+    from wsgiserver.template import Template, Context
 
+    template = Template('<html>{% if test %}<h1>{{ varvalue }}</h1>{% endif %}</html>')
+    print template.__dict__
+    # context = Context({'test': True, 'varvalue': 'Hello'})
+    # print template.render(context)
