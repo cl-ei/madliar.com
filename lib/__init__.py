fcc28b7fefec062401e6460ca96d3d69a22ad9fd
Caoliang <i@caoliang.net>
2017-07-25 10:14:11 +0800

    add get_ip_tool

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,2 +1,9 @@
 from . import *
 from .command import *
+
+
+def get_ip():
+    import socket
+    myname = socket.getfqdn(socket.gethostname())
+    myaddr = socket.gethostbyname(myname)
+    return myname
