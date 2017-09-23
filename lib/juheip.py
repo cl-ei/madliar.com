74dd5c728afbeafdc26cb21fb3d8c0b4fd990a92
Caoliang <i@caoliang.net>
2017-09-23 15:37:10 +0800

    add juheip lib

---
+++ lib/juheip.py
@@ -0,0 +1,32 @@
+import json
+import urllib
+from urllib import urlencode
+
+
+appkey = "f551cc9ac9dad658b231006f58965077"
+
+
+def get_ip_info(ip):
+    url = "http://apis.juhe.cn/ip/ip2addr"
+    params = {
+        "ip": ip,
+        "key": appkey,
+        "dtype": "json",
+    }
+    params = urlencode(params)
+    f = urllib.urlopen("%s?%s" % (url, params))
+    content = f.read()
+    res = json.loads(content)
+    print res
+
+    err_code = res.get("error_code", 0)
+    if err_code == 0:
+        data = res.get("result", {}).get("area")
+    else:
+        data = res.get("reason")
+    return err_code, data
+
+
+if __name__ == '__main__':
+    test_ip = "106.11.242.36"
+    print "%s, %s" % get_ip_info(test_ip)
